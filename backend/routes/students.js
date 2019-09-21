74e5a3abf7fe7c95ebd3ea08a73a222a93e07a8c
KJM9626 <48523619+KJM9626@users.noreply.github.com>
2019-09-22 01:08:23 +0800

    Update students.js

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -8,7 +8,7 @@ router.get('/getAll',async(ctx,next)=>{
         message:'fail',
         data:[]
     }
-    await knex('student')
+    await knex('students')
     .join('room','student.room_id','=','room.id')
     .select('student.id','student.name as name','gender','student.dorm_id','room.name as room').then(e=>{
       if(e.length === 0){
@@ -25,7 +25,7 @@ router.get('/getAll',async(ctx,next)=>{
 router.post('/add',async(ctx,next)=>{
   let req = ctx.request.body // req: name,gender,dorm_id,room_id
   let back = 'fail'
-  await knex('student').insert({
+  await knex('students').insert({
     name:req.name,
     gender:req.gender,
     dorm_id:req.dorm_id,
