53d70785f8633cf9a9297d7cc5b323c6538d2fa7
2441020081 <43643720+2441020081@users.noreply.github.com>
2019-09-22 16:38:27 +0800

    Update rooms.vue

--- src/views/contents/rooms.vue
+++ src/views/contents/rooms.vue
@@ -3,7 +3,18 @@
         <Modal v-model="detailModal" title="所住学生">
             <Table :columns="columns2" :data="data2"></Table>
         </Modal>
-        <Button type="primary" style="align:left;margin-bottom:15px;">新增房间</Button>
+        <Modal
+            v-model="addModal3"
+            title="新增房间"
+            @on-ok="submit"
+            @on-cancel="addModal3 = false"
+            align="center">
+            <Input v-model="addInfo.id" placeholder="请输入房间ID" style="width:200px"/><br>
+            <Input v-model="addInfo.name" placeholder="请输入房间号" style="width:200px"/><br>
+            <Input v-model="addInfo.dorm_id" placeholder="请输入楼号" style="width:200px"/><br>
+            <Input v-model="addInfo.max" placeholder="请输入最大容纳数量" style="width:200px"/>
+        </Modal>
+        <Button type="primary" style="align:left;margin-bottom:15px;" @click="addModal3 = true">新增房间</Button>
         <Table :columns="columns1" :data="data1"></Table>
     </div>
 </template>
@@ -20,8 +31,20 @@
                 }
             })
         },
+        methods: {
+            submit(){
+                
+            }
+        },
         data () {
             return {
+                addInfo:{
+                    id:null,
+                    name:null,
+                    dorm_id:null,
+                    max:null
+                },
+                addModal3:false,
                 detailModal:false,
                 columns2: [
                     {
